677f080e15f7e8f1b72476961081bdd09299d203
aaq-and-dev <aaqjavadevmentor@gmail.com>
2021-05-01 16:31:48 -0700

    * adds Service to use message bean, and provide modifying methods

--- src/main/java/com/aaqanddev/jwdnd/c1/review/MessageService.java
+++ src/main/java/com/aaqanddev/jwdnd/c1/review/MessageService.java
@@ -1,2 +1,23 @@
-package com.aaqanddev.jwdnd.c1.review;public class MessageService {
+package com.aaqanddev.jwdnd.c1.review;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Locale;
+
+@Service
+public class MessageService {
+    private String message;
+
+    public MessageService(String message){
+        System.out.println("messageService bean created");
+        this.message = message;
+    }
+
+    public String uppercase(){
+        return message.toUpperCase(Locale.ROOT);
+    }
+
+    public String lowercase(){
+        return message.toLowerCase(Locale.ROOT);
+    }
 }
